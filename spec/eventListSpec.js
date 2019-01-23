ebb780956e3b12b13dd753560611aaeb483215d1
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-01-23 14:27:36 +0000

    Failing test written for storing event

--- spec/eventListSpec.js
+++ spec/eventListSpec.js
@@ -1,13 +1,13 @@
 describe('Event List', function() {
-  var eventList
-  var event
+  var eventList = new EventList()
+  var event = new Event("Subomi's fantastic jamboree", "22/01/2019", '18:45')
 
   testWill('check that a new instance of event has an empty array', function() {
-    eventList = new EventList()
     expect(eventList.entries.length).toBeEqualTo(0)
   })
 
   testWill('check that events can be stored in EventList array', function() {
-
+    eventList.storeEvent(event)
+    expect(eventList.entries[0]).toBeEqualTo(event)
   })
 })
--- src/eventList.js
+++ src/eventList.js
@@ -2,4 +2,3 @@ class EventList {
   constructor() {
     this.entries = []
   }
-}

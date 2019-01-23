015aeb5631b2d64472cea87fb6a038aa5b0cafbc
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-01-23 10:57:15 +0000

    Failing test written for time of event

--- spec/eventgerSpec.js
+++ spec/eventgerSpec.js
@@ -10,4 +10,9 @@ describe('Eventger', function() {
     event = new Event("Subomi's fantastic jamboree", "22/01/2019")
     expect(event.date).toBeEqualTo('22/01/2019')
   })
+
+  testWill('check that an event has a time', function() {
+    event = new Event("Subomi's fantastic jamboree", "22/01/2019", "18:45")
+    expect(event.time).toBeEqualTo('18:45')
+  })
 })

c57f5ab687f42d2d7f952409e00eb5271b63c69e
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-01-23 11:05:11 +0000

    Time added and can be displayed on event planner

--- src/interface.js
+++ src/interface.js
@@ -1,14 +1,15 @@
 function listEvent() {
-  var newEvent = document.getElementById("desc_id");
-  var newDate = document.getElementById("date_id");
-  var send = document.getElementById("button");
-  var eventHistory = document.getElementById("eventHistory");
+  var newEvent = document.getElementById("desc_id")
+  var newDate = document.getElementById("date_id")
+  var newTime = document.getElementById("time_id")
+  var send = document.getElementById("button")
+  var eventHistory = document.getElementById("eventHistory")
   send.onclick = function() {
-    var event = new Event(newEvent.value, newDate.value);
-    var displayEvent = document.createElement("ol");
-    displayEvent.innerHTML = event.desc + " - " + event.date
-    eventHistory.prepend(displayEvent);
-    newEvent.value = "";
+    var event = new Event(newEvent.value, newDate.value,newTime.value)
+    var displayEvent = document.createElement("ol")
+    displayEvent.innerHTML = event.desc + " - " + event.date + " - " + event.time
+    eventHistory.prepend(displayEvent)
+    newEvent.value = ""
   }
 }
 document.onload = listEvent()
